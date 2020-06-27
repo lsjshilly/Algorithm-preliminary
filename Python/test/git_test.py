4aabb58c1d6d308b6329118bc8db7e41663de05f
Liushijie <Liushijie@163.com>
2020-06-27 16:53:11 +0800

    first test python code

--- Python/test/git_test.py
+++ Python/test/git_test.py
@@ -5,6 +5,7 @@ def main():
     print(time.ctime(time.time()))
     print("Hello, this is my first python code test!")
     print("add line again")
+    print("add line twice")
 
 
 if __name__ == "__main__":
