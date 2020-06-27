634397d59d0078afe39959af8b4d5d91d61c5220
Liushijie <Liushijie@163.com>
2020-06-27 16:16:33 +0800

    first test python code

---
+++ Python/test/git_test.py
@@ -0,0 +1,11 @@
+import time
+
+
+def main():
+    print(time.ctime(time.time()))
+    print("Hello, this is my first python code test!")
+    print("add line again")
+
+
+if __name__ == "__main__":
+    main()
