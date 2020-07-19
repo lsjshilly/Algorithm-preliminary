86bee3053141933bd33fe63f737ae6b014fdec74
Liushijie <Liushijie@163.com>
2020-07-19 18:18:41 +0800

    第四章第一节A

---
+++ C++/4_1/A.cpp
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<algorithm>
+using namespace std;
+int main(){
+    int n;
+    int a[101];
+
+    while(scanf("%d", &n)!=EOF){
+        for(int i=0;i<n;i++){
+            scanf("%d", a+i);
+        }
+        sort(a, a+n);
+        for(int i=0;i<n;i++){
+            printf("%d ", a[i]);
+        }
+        printf("\n");
+    }
+}
